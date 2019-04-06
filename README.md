08ee9d9f44d74818ff34261078f9aaa854a4792a
jakubowiczish <jakub77plotnikowski@gmail.com>
2019-04-07 01:24:01 +0200

    Update readme image

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ## My solutions for Computation Methods for Science and Technology 2 assignment
 
-![alt text](https://raw.githubusercontent.com/jakubowiczish/mownit/master/sylvan.png)
+![alt text](https://raw.githubusercontent.com/jakubowiczish/mownit/master/bubu.png)
---
+++ cmake-build-debug/CMakeFiles/mownit.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mownit.dir/laboratory1/main.c.o"
+  "CMakeFiles/mownit.dir/laboratory2/main.c.o"
+  "CMakeFiles/mownit.dir/laboratory3/main.c.o"
+  "mownit.pdb"
+  "mownit"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/mownit.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
